3722ac4aafc48fd62ec946911a35e571e1c7b9b2
Hoan Nguyen <ndhoan1995@gmail.com>
2024-02-27 11:40:58 +0700

    implement parallax section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         Karla: ["Karla", "sans-serif"],
